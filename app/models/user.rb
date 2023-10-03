6e50dea66c84cb2bb83a9914614cc2865f37afd3
davislucky <58579323+davislucky@users.noreply.github.com>
2023-10-03 16:27:04 -0700

    Seed users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,52 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :bigint           not null, primary key
+#  first_name      :string           not null
+#  last_name       :string           not null
+#  email           :string           not null
+#  password_digest :string           not null
+#  session_token   :string           not null
+#  birth_date      :date             not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
 class User < ApplicationRecord
+    validates :first_name, :last_name, :birth_date, :password_digest, presence: true
+    validates :session_token, presence: true, uniqueness: true
+    validates :password, length: { minimum: 6 }, allow_nil: true
+    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }, presence: true, uniqueness: true
+    before_validation :ensure_session_token
+
+    has_secure_password
+
+    def self.find_by_credentials(email, password)
+        user = User.find_by(email: email)
+
+        if user && user.authenticate(password)
+            return user
+        else
+            nil
+        end
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        self.save!
+        self.session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        loop do
+            token = SecureRandom::urlsafe_base64
+            return token unless User.exists?(session_token: token)
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+ApplicationRecord.transaction do
+    puts "Destroying tables..."
+    User.destroy_all
+
+    puts "Resetting primary keys..."
+    ApplicationRecord.connection.reset_pk_sequence!('users')
+
+    puts "Creating users..."
+    User.create!(
+        email: 'demo@user.io',
+        password: 'password',
+        first_name: 'Demo',
+        last_name: 'User',
+        birth_date: '2023-10-03'
+    )
+    User.create!(
+        email: 'davislucky@user.io',
+        password: 'password',
+        first_name: 'Davis',
+        last_name: 'Lucky',
+        birth_date: '1997-03-04'
+    )
+    User.create!(
+        email: 'julia@tennis.io',
+        password: 'password',
+        first_name: 'Julia',
+        last_name: 'Kahn',
+        birth_date: '2002-04-05'
+    )
+    User.create!(
+        email: 'shannon@uc.edu',
+        password: 'password',
+        first_name: 'Shannon',
+        last_name: 'Millar',
+        birth_date: '1998-09-24'
+    )
+    User.create!(
+        email: 'mitchell@merch.com',
+        password: 'password',
+        first_name: 'Mitchell',
+        last_name: 'Chan',
+        birth_date: '1994-07-15'
+    )
+    puts "Done!"
+end
