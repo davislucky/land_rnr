b8acfcb0a9e55ab7312ea60dfe48ed16e149073e
davislucky <58579323+davislucky@users.noreply.github.com>
2023-10-09 23:18:37 -0700

    Add github and linked-in icons to navbar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,6 +1,11 @@
 import React from "react";
-import './NavBar.css'
-import logo from '../../assets/logo.png'
+import './NavBar.css';
+import logo from '../../assets/logo.png';
+import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
+
 
 const navbar = () => {
     return (
@@ -10,8 +15,19 @@ const navbar = () => {
                 <div className="search-bar-text">Any where</div>
                 <div className="search-bar-text">Any week</div>
                 <div className="search-bar-text2">Add guests</div>
+                <div className="search-icon-div">
+                    <SearchRoundedIcon className="search-icon-div"/>    
+                </div>
             </div>
-            <div className="session-container"></div>
+            <div className="session-container">
+                <div className="github-link">
+                    <FontAwesomeIcon className="github-icon" icon={faGithub} style={{color: "#ff2600",}} />
+                </div>
+                <div className="linkedin-link">
+                    <FontAwesomeIcon className="linkedin-icon" icon={faLinkedin} style={{color: "#ff2600",}} />
+                </div>
+                <div className="profile-dropdown"></div>
+            </div>
         </div>
     )
 }
